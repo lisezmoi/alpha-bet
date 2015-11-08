74e2864cd156dc18c38c6f980570d21e196eca7b
raphaelbastide <raphael.bastide@gmail.com>
2015-11-08 02:06:13 +0100

    Anim, highlighter function and avatar update

--- mockup/js/main.js
+++ mockup/js/main.js
@@ -3,3 +3,23 @@
 $('.about-button').click(function(){
     $('#about').toggleClass('opened');
 });
+
+var lastLine = $('.last-line'),
+    userList = $('.user-list'),
+    userChar = $('.myself .letter').html();
+function tagHighlight(){
+  lastLine.addClass('highlight');
+  // userList.removeClass('highlight')
+};
+function highlight(currentChar, otherChar){
+  lastLine.html(lastLine.text().split('').map(char => {
+    var classCurrent = char === currentChar? 'letter-current':'';
+    var classOther = otherChar.indexOf(char) > -1? 'letter-other':'';
+    return `<span class="letter ${classCurrent}${classOther}">${char}</span>`
+  }).join(''));
+  if (userChar === currentChar) {
+  }
+    userList.addClass('highlight');
+  setTimeout('tagHighlight()', 1);
+}
+highlight('b',['s','e','i','n']);
