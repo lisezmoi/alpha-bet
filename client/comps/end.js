036cff9d6b60a57939090d3613660b55ad644fa7
raphaelbastide <raphael.bastide@gmail.com>
2015-11-09 00:35:00 +0100

    Vote button on the end screen

--- client/comps/end.js
+++ client/comps/end.js
@@ -4,6 +4,15 @@ const End = props => (
   <div className='End'>
     <p>Game over</p>
     <p><a href='/'>Try again</a></p>
+    <iframe
+      className='support end'
+      src='http://nodeknockout.com/iframe/lisezmoi'
+      frameBorder='0'
+      scrolling='no'
+      allowTransparency='true'
+      width='115'
+      height='25'
+    />
   </div>
 )
 
