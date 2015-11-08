92d2a5cd756ee73427d2a9b44ba385d0128eb13e
raphaelbastide <raphael.bastide@gmail.com>
2015-11-08 17:01:32 +0100

    Quick mobile version

--- client/comps/about.js
+++ client/comps/about.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const About = props => (
   <div className={'About' + (props.opened? ' About-opened' : '')}>
+    Alpha Bet <br/>
     A project by <a href='http://lisezmoi.org/'>LISEZMOI</a>
     <br/>
     for Node.js Knockout 2015
@@ -9,6 +10,15 @@ const About = props => (
     <a href='https://github.com/lisezmoi/alpha-bet'>
       Source
     </a>
+    <iframe
+      className='support mobile'
+      src='http://nodeknockout.com/iframe/lisezmoi'
+      frameBorder='0'
+      scrolling='no'
+      allowTransparency='true'
+      width='115'
+      height='25'
+    />
     <span
       className='about-button'
       onClick={props.onToggleAbout}
--- client/comps/sidebar.js
+++ client/comps/sidebar.js
@@ -4,12 +4,12 @@ import Users from './users'
 
 const Sidebar = props => (
   <div className='Sidebar'>
-    <h2>Press and hold on a letter to bet</h2>
+    <h2 className='title-letters'>Press and hold on a letter to bet</h2>
     <Letters
       onStartBet={props.onStartBet}
       onEndBet={props.onEndBet}
     />
-    <h2>Participants</h2>
+  <h2 className='title-users'>Participants</h2>
     <Users
       users={props.users}
     />
